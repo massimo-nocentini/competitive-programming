46ebebe07411596214020b2504ab1a33689edc79
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-05-10 17:57:55 +0200

    Refactor UVa 11559 and add an utility to loop forever except an event is raised.

--- UVa/11559.py
+++ UVa/11559.py
@@ -14,34 +14,37 @@ def line_bind(line, *args, splitter=' '):
 @contextmanager
 def stdin_input():
     yield fileinput.input()
+
+def forever_read_until_event(fn, event=StopIteration):
+    with stdin_input() as f:
+        while True:
+            try: fn(f)
+            except event: break
 #_________________________________________________________________________
 
-with stdin_input() as f:
 
-    while True:
+def reader(f):
 
-        try:
+    inf = float("+inf")
+    cheaper = inf 
 
-            inf = float("+inf")
-            cheaper = inf 
+    with line_bind(next(f), int, int, int, int) as (n, b, h, w):
 
-            with line_bind(next(f), int, int, int, int) as (n, b, h, w):
+        for _ in range(h):
 
-                for _ in range(h):
+            with line_bind(next(f), int) as (p,), \
+                 line_bind(next(f), *([int] * w)) as beds:
 
-                    with line_bind(next(f), int) as (p,), \
-                         line_bind(next(f), *([int] * w)) as beds:
+                cost = n * p
 
-                        cost = n * p
+                if cost > b or all(map(lambda bs: n > bs, beds)): continue
+                
+                cheaper = min(cheaper, cost)
 
-                        if cost > b or all(map(lambda bs: n > bs, beds)): continue
-                        
-                        cheaper = min(cheaper, cost)
+    print("stay home" if cheaper == inf else str(cheaper))
 
-            print("stay home" if cheaper == inf else str(cheaper))
-
-        except StopIteration as e: break
-
+# do the planning
+forever_read_until_event(reader)
 
 
 
--- UVa/utilities.py
+++ UVa/utilities.py
@@ -1,6 +1,7 @@
 
 # ___declarations in `utilities.py`___
 # do update in `utilities.py` if you do any change
+#_________________________________________________________________________
 
 import fileinput
 from contextlib import contextmanager
@@ -12,4 +13,14 @@ def line_bind(line, *args, splitter=' '):
 @contextmanager
 def stdin_input():
     yield fileinput.input()
-#_________________________________________________________________________
+
+def forever_read_until_event(fn, event=StopIteration):
+    with stdin_input() as f:
+        while True:
+            try: fn(f)
+            except event: break
+
+
+
+
+
