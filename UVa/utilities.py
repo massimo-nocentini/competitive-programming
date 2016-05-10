25a07a185db9fea2de9518be29bd4b283aa0bf5d
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-05-10 22:52:13 +0200

    Add two functions to test order property of a list, both asc and dec orders can be checked.

--- UVa/utilities.py
+++ UVa/utilities.py
@@ -20,6 +20,20 @@ def forever_read_until_event(fn, event=StopIteration):
             try: fn(f)
             except event: break
 
+#________________________________________________________________________
+
+# Look at [1] and [2]:
+# [1]:http://stackoverflow.com/questions/3755136/pythonic-way-to-check-if-a-list-is-sorted-or-not
+# [2]:http://stackoverflow.com/questions/12734178/determine-if-a-list-is-in-descending-order
+
+def is_non_increasing(l):
+    for i, el in enumerate(l[1:], start=1):
+        if el >= l[i-1]: return False
+    return True
+
+def is_non_decreasing(l):
+    return all(x <= y for x,y in zip(l, l[1:]))
+
 
 
 
