928109570ec32959046105bcd84c0c560f0d67f2
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-05-10 14:15:30 +0200

    Solved UVa 10114.

--- UVa/10114.py
+++ UVa/10114.py
@@ -1,43 +1,47 @@
 
 # https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=608&page=show_problem&problem=1055
 
+import collections
 import fileinput
+from contextlib import contextmanager
+
+@contextmanager
+def line_bind(line, *args, splitter=' '):
+    yield (c(v) for c, v in zip(args, line.split(splitter)))
 
 with fileinput.input() as f:
 
     while True:
-        line = next(f).split(' ')
-        duration, down_pay, load, depr_nel = int(line[0]), float(line[1]), float(line[2]), int(line[3])
-        if duration < 0: 
-            break
 
-        buyer_owes = load
-        line = next(f).split(' ')
-        off_lot, off_lot_perc = int(line[0]), float(line[1])
-        car_value = (load + down_pay) * (1 - off_lot_perc)
+        with line_bind(next(f), int, float, float, int) as (duration, down_pay, loan, depr_nel):
 
-        deprs = [off_lot_perc] + ([0] * duration)
-        last = 0 
-        for d in range(depr_nel-1):
-            line = next(f).split(' ')
-            month, perc = int(line[0]), float(line[1])
-            for i in range(last+1, month):
+            if duration < 0: break
+
+            deprs = [0] * (duration + 1)
+            last = -1 
+            for d in range(depr_nel):
+                with line_bind(next(f), int, float) as (month, perc):
+                    for i in range(last+1, month): deprs[i]=deprs[last]
+                    deprs[month], last = perc, month
+            for i in range(last+1, len(deprs)):
                 deprs[i]=deprs[last]
-            deprs[month]=perc
-            last = month
-        for i in range(last+1, len(deprs)):
-            deprs[i]=deprs[last]
-        deprs[0] = off_lot_perc
 
-        #print(deprs)
-        for month in range(1, len(deprs)):
-            buyer_owes -= down_pay 
-            car_value *= 1 - deprs[month]
-            print('buyer: {} car: {}'.format(buyer_owes, car_value))
-            if buyer_owes < car_value: 
-                break
+            buyer_owes, payment = loan, loan / duration
+            car_value = (loan + down_pay) * (1 - deprs[0])
 
-        print('{} month{}'.format(month, '' if month == 1 else 's'))
+            if loan < car_value:
+                print('0 months')
+                continue
+
+            for month in range(1, len(deprs)):
+                buyer_owes -= payment 
+                car_value -= car_value*deprs[month]
+                #print('month: {} buyer: {} car: {}'.format(
+                    #month, buyer_owes, car_value))
+                if buyer_owes < car_value: 
+                    break
+
+            print('{} month{}'.format(month, '' if month == 1 else 's'))
 
 
 
