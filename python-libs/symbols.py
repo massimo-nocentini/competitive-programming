8dcd31e7b5448608b842de97208bb885252c18bf
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-05-25 19:04:08 +0200

    Refactor and commented Polyominoes problem.

---
+++ python-libs/symbols.py
@@ -0,0 +1,16 @@
+
+def lower_greek_symbols(plain_string=True):
+    """
+    Returns an iterable of *lower* Greek symbols. 
+    
+    Built by Vim's `:help digraph-table@en` command.
+    """
+    return 'αβγδεζηθικλμνξοπρςστυφχψω'
+
+def box_drawings_light(piece_name):
+    """
+    Return box symbol named `piece_name`, according to Vim's digraph table.
+    """
+    pass
+
+    
