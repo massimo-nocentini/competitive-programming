e61df6034642d1e0cebac69725ab40366810d0ec
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-06-10 18:13:25 +0200

    Write another pretty printer that uses shapes to represent tilings instead of symbols.

--- python-libs/symbols.py
+++ python-libs/symbols.py
@@ -15,6 +15,14 @@ def capital_greek_symbols():
     """
     return "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ"
 
+def latin_symbols():
+    """
+    Returns an iterable of Latin symbols. 
+    
+    Built by Vim's `:help digraph-table@en` command.
+    """
+    return "ḂḃḆḇḊḋḎḏḐḑḞḟḠḡḢḣḦḧḨḩḰḱḴḵḺḻḾḿṀṁṄṅṈṉṔṕṖṗṘṙṞṟṠṡṪṫṮṯṼṽẀẁẂẃẄẅẆẇẊẋẌẍẎẏẐẑẔẕẖẗẘẙẢảẺẻẼẽỈỉỎỏỦủỲỳỶỷỸỹ"
+
 def box_drawings_light(piece_name):
     """
     Return box symbol named `piece_name`, according to Vim's digraph table.
